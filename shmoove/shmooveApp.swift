cecc0a2e80914e36da278e559b3d8c2e44d89e20
Jayy <31707578+weilwegenlogik@users.noreply.github.com>
2024-05-05 18:52:16 +0200

    Initial Commit

---
+++ shmoove/shmooveApp.swift
@@ -0,0 +1,17 @@
+//
+//  shmooveApp.swift
+//  shmoove
+//
+//  Created by Jakob Schulze on 05.05.24.
+//
+
+import SwiftUI
+
+@main
+struct shmooveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
